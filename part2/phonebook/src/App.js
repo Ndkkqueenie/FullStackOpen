2c6ffd7ae004319fcc5a28b26329ee00eff8d33b
Queendalin Ezenwa <queendalin.nduka@gmail.com>
2023-07-13 18:57:56 +0200

    pert2/c - data frome server exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Form from './components/Form';
 import SearchField from './components/SearchField';
 import NumbersList from './components/NumbersList';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchName, setSearchName] = useState('');
+
+  const hookEffect = () => {
+    console.log('Effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hookEffect, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
