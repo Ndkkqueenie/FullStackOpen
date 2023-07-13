47f675f39b7570bbce1ca39678976ed526b4d486
Queendalin Ezenwa <queendalin.nduka@gmail.com>
2023-07-13 16:15:15 +0200

    coursework part2/b - server-side implementation

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note';
 
-const App = ({ notes }) => {
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const hookEffect = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3005/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hookEffect, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -2,26 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
-const result = notes.map(note => note.id)
-console.log(result)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
+  <App/>
 )
