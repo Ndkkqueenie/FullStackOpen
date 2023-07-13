4269445e571e9726376d65fb93e113cd69233d11
Queendalin Ezenwa <queendalin.nduka@gmail.com>
2023-07-13 20:17:49 +0200

    toggle important

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -34,8 +34,14 @@ const App = () => {
     })
   };
 
-  const toggleImportanceOf = (id) => {
-    console.log(`importance of ${id} needs to be toggled`)
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3005/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
   };
 
   const handleNoteChange = (event) => {
